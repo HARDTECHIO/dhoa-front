087d95bd04bbff3997c245563578af7e4ff803cb
Milton-668 <miltonmjcp@gmail.com>
2021-04-08 17:26:07 -0300

    feat(api): Implementado o metodo de atualização e deleção em  postagens #14

--- src/app/views/principal/deletar/deletar-postagens/deletar-postagens.component.ts
+++ src/app/views/principal/deletar/deletar-postagens/deletar-postagens.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/models/Postagem';
+import { PostagemService } from 'src/app/service/postagem.service';
 
 @Component({
   selector: 'app-deletar-postagens',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeletarPostagensComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
 
-  ngOnInit(): void {
+  idPost: number
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(){
+
+    window.scroll(0, 0)
+
+    this.idPost = this.route.snapshot.params['id']
+
+    this.getPostagem(this.idPost)
+  }
+
+  getPostagem(id: number) {
+    this.postagemService.findById(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+
+  }
+
+  apagar(){
+    this.postagemService.delete(this.idPost).subscribe(()=>{
+      alert('Sua postagem foi apagada com sucesso!')
+      this.router.navigate(['/minhas-postagens'])
+    })
   }
 
 }
