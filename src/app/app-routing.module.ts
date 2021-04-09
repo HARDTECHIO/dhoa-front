ae490ea363fbd9b7996ac1069eb0c382090baa60
ShenSouza <ds052798ts@gmail.com>
2021-04-09 10:54:33 -0300

    chore(merge): Subindo a nova branch para o trabalho #16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './views/cadastrar/cadastrar.component';
 import { EntrarComponent } from './views/entrar/entrar.component';
 import { CategoriasComponent } from './views/principal/categorias/categorias.component';
-<<<<<<< HEAD
 import { DeletarPostagensComponent } from './views/principal/deletar/deletar-postagens/deletar-postagens.component';
-import { EditarPostagensComponent } from './views/principal/editar/editar-postagens/editar-postagens.component';
 import { DeletarCategoriasComponent } from './views/principal/deletar/deletar-categorias/deletar-categorias.component';
 import { EditarCategoriasComponent } from './views/principal/editar/editar-categorias/editar-categorias.component';
-=======
-import { DeletarCategoriasComponent } from './views/principal/deletar/deletar-categorias/deletar-categorias.component';
-import { EditarCategoriasComponent } from './views/principal/editar/editar-categorias/editar-categorias.component';
-import { DeletarPostagensComponent } from './views/principal/deletar/deletar-postagens/deletar-postagens.component';
 import { EditarPostagensComponent } from './views/principal/editar/editar-postagens/editar-postagens.component';
->>>>>>> d255b9a331899f4c98ad18f2303dbc2a43219ee6
 import { InicioComponent } from './views/principal/inicio/inicio.component';
 import { MinhasPostagensComponent } from './views/principal/minhas-postagens/minhas-postagens.component';
 import { PerfilComponent } from './views/principal/perfil/perfil.component';
@@ -21,28 +14,25 @@ import { SiteComponent } from './views/site/site.component';
 
 const routes: Routes = [
   // Rotas não logado
-  {path: 'site', component: SiteComponent},
-  {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent},
+  { path: 'site', component: SiteComponent },
+  { path: 'entrar', component: EntrarComponent },
+  { path: 'cadastrar', component: CadastrarComponent },
 
   // Rotas do usuário logado
-  {path: '', component: InicioComponent},
-  {path: 'perfil', component: PerfilComponent},
-  {path: 'minhas-postagens', component: MinhasPostagensComponent},
-  {path: 'postagem/editar/:id', component : EditarPostagensComponent},
-  {path: 'postagem/apagar/:id', component: DeletarPostagensComponent},
+  { path: '', component: InicioComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'minhas-postagens', component: MinhasPostagensComponent },
+  { path: 'postagem/editar/:id', component: EditarPostagensComponent },
+  { path: 'postagem/apagar/:id', component: DeletarPostagensComponent },
 
   //Rotas Usuarios ADM
-  {path: 'categorias', component: CategoriasComponent},
-<<<<<<< HEAD
-
-  
+  { path: 'categorias', component: CategoriasComponent },
 
 
-=======
->>>>>>> d255b9a331899f4c98ad18f2303dbc2a43219ee6
-  {path: 'categorias/editar/:id', component: EditarCategoriasComponent},
-  {path: 'categorias/apagar/:id', component: DeletarCategoriasComponent}
+
+
+  { path: 'categorias/editar/:id', component: EditarCategoriasComponent },
+  { path: 'categorias/apagar/:id', component: DeletarCategoriasComponent }
 ];
 
 @NgModule({
