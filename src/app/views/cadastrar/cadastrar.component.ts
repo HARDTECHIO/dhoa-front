483488c2d22299d9b45156ab9438a6cb2464efac
Jecesario <jp@soloid.com.br>
2021-04-12 17:39:47 -0300

    fix: Corrigido botões pedir e oferecer ajuda

--- src/app/views/cadastrar/cadastrar.component.ts
+++ src/app/views/cadastrar/cadastrar.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../../service/auth.service';
 import { Usuario } from './../../models/Usuario';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cadastrar',
@@ -12,7 +13,12 @@ export class CadastrarComponent implements OnInit {
 
   usuario: Usuario = new Usuario
 
-  confirmarSenha: string
+  nomeValido = false
+  emailValido = false
+  fotoValida = false
+  senhaValida = false
+  confirmaSenha = false
+
 
   constructor(
     private authService: AuthService,
@@ -23,19 +29,74 @@ export class CadastrarComponent implements OnInit {
     window.scroll(0,0)
   }
 
-  confirmSenha(event: any){
-    this.confirmarSenha = event.target.value
+  validaNome(event: any) {
+    this.nomeValido = this.validar(event.target.value.length < 2 || event.target.value.length > 100, event)
+  }
+
+  validaEmail(event: any) {
+    this.emailValido = this.validar(event.target.value.indexOf('@') == -1 || event.target.value.indexOf('.') == -1, event)
+  }
+
+  validaFoto(event: any) {
+    let regex = /\.(jpe?g|png)$/i
+    this.fotoValida = this.validar(!regex.test(event.target.value), event)
+  }
+
+  validaSenha(event: any) {
+    this.senhaValida = this.validar(event.target.value.length < 5 || event.target.value.length > 40, event)
+  }
+
+  validaConfirmaSenha(event: any) {
+    this.confirmaSenha = this.validar(event.target.value.length < 5 || event.target.value.length > 40, event)
+  }
+
+  validar(condicao: boolean, event: any) {
+    let valido = false
+    if (condicao) {
+      event.target.classList.remove("is-valid")
+      event.target.classList.add("is-invalid")
+    } else {
+      event.target.classList.remove("is-invalid")
+      event.target.classList.add("is-valid")
+      valido = true
+    }
+    return valido
   }
 
   cadastrar(){
-    if(this.usuario.senha != this.confirmarSenha){
-      alert('As senhas estão incorretas!!!')
-    }else{
-      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario)=>{
-        this.usuario = resp
-        this.router.navigate(['/entrar'])
-        alert('Cadastrado com sucesso!!')
+    if(this.senhaValida != this.confirmaSenha){
+      Swal.fire({
+        icon: 'error',
+        title: 'Ocorreu um erro',
+        text: 'As senhas não conferem',
+        showConfirmButton: false,
+        timer: 2000
       })
+    } else{
+      if(this.nomeValido && this.emailValido && this.senhaValida && this.confirmaSenha) {
+        if(this.usuario.imagemUrl == '' || this.usuario.imagemUrl == null) {
+          this.usuario.imagemUrl = 'https://i.imgur.com/wLwPeGf.png'
+        }
+        this.authService.cadastrar(this.usuario).subscribe((resp: Usuario)=>{
+          this.usuario = resp
+          this.router.navigate(['/entrar'])
+          Swal.fire({
+            icon: 'success',
+            title: 'Muito bom',
+            text: 'Usuário cadastrado com sucesso',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        })
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ocorreu um erro',
+          text: 'Preencha os campos corretamente',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      }
     }
   }
 
--- src/app/views/principal/inicio/inicio.component.ts
+++ src/app/views/principal/inicio/inicio.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from 'src/app/service/auth.service';
 import { CategoriaService } from 'src/app/service/categoria.service';
 import { PostagemService } from 'src/app/service/postagem.service';
 import { environment } from 'src/environments/environment.prod';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio',
@@ -31,7 +32,9 @@ export class InicioComponent implements OnInit {
   key = 'dataPostagem'
   reverse = true
 
-
+  // Variaveis para validaçõa de nova postagem
+  textoValido = false
+  imagemValida = false
 
   constructor(
     private postagemService: PostagemService,
@@ -44,11 +47,42 @@ export class InicioComponent implements OnInit {
     this.listarPostagens()
   }
 
+  validaTexto(event: any) {
+    this.textoValido = this.validar(event.target.value.length < 4 || event.target.value.length > 255, event)
+  }
+
+  validaImagem(event: any) {
+    let regex = /\.(jpe?g|png)$/i
+    this.imagemValida = this.validar(!regex.test(event.target.value), event)
+  }
+
+  limparValidacoes() {
+    this.textoValido = false
+    this.imagemValida = false
+    let texto = document.querySelector('#texto')
+    texto?.classList.remove('is-valid')
+    let imagem = document.querySelector('#imagemUrl')
+    imagem?.classList.remove('is-valid')
+  }
+
+  validar(condicao: boolean, event: any) {
+    let valido = false
+    if (condicao) {
+      event.target.classList.remove("is-valid")
+      event.target.classList.add("is-invalid")
+    } else {
+      event.target.classList.remove("is-invalid")
+      event.target.classList.add("is-valid")
+      valido = true
+    }
+    return valido
+  }
+
   listarPostagens() {
     this.postagemService.findAll().subscribe((resp: Postagem[]) => {
       this.postagens = resp
-      // let all = document.querySelector('#todasPostagens')
-      // all?.classList.add('btn-verde-ativo')
+      let all = document.querySelector('#todasPostagens')
+      all?.classList.add('btn-verde-ativo')
     })
   }
 
@@ -58,7 +92,7 @@ export class InicioComponent implements OnInit {
     })
   }
 
-  publicar() {
+  publicarP() {
     this.categoria.id = this.idCategoria
     this.postagem.categoria = this.categoria
 
@@ -67,16 +101,75 @@ export class InicioComponent implements OnInit {
 
     this.postagem.contatoUrl = this.emailUsuario
 
-    let tipoPostagem = (document.querySelector('#tipoPostagem') as HTMLInputElement).value
+    let tipoPostagem = (document.querySelector('#tipoPostagemP') as HTMLInputElement).value
     this.postagem.tipoPostagem = tipoPostagem
 
-    this.postagemService.create(this.postagem).subscribe((resp: Postagem) => {
-      this.postagem = resp
-      alert('Postagem realizada com sucesso')
-      this.postagem = new Postagem()
-      this.listarPostagens()
-    })
+    if(this.textoValido && this.imagemValida) {
+      this.postagemService.create(this.postagem).subscribe((resp: Postagem) => {
+        this.postagem = resp
+        Swal.fire({
+          icon: 'success',
+          title: 'Muito bom',
+          text: 'Postagem realizada com sucesso',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.postagem = new Postagem()
+        this.limparValidacoes()
+        this.listarPostagens()
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Ocorreu um erro',
+        text: 'Preencha os campos corretamente',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }
+
+    
   }
+
+  publicarO() {
+    this.categoria.id = this.idCategoria
+    this.postagem.categoria = this.categoria
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagem.contatoUrl = this.emailUsuario
+
+    let tipoPostagem = (document.querySelector('#tipoPostagemO') as HTMLInputElement).value
+    this.postagem.tipoPostagem = tipoPostagem
+
+    if(this.textoValido && this.imagemValida) {
+      this.postagemService.create(this.postagem).subscribe((resp: Postagem) => {
+        this.postagem = resp
+        Swal.fire({
+          icon: 'success',
+          title: 'Muito bom',
+          text: 'Postagem realizada com sucesso',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.postagem = new Postagem()
+        this.limparValidacoes()
+        this.listarPostagens()
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Ocorreu um erro',
+        text: 'Preencha os campos corretamente',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }
+
+    
+  }
+
   findByIdUser() {
     this.authService.findById(this.idUsuario).subscribe((resp: Usuario) => {
       this.usuario = resp
